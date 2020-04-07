67c695df14ba9efe509a4626e427bf97fb8337e0
Tonic78 <guymertz@gmaiL.com>
2020-04-07 14:52:56 +0200

    Added useEffect and Dispatch to CheckoutPage

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchShoppingCardById } from "../../store/shoppingcard/actions";
+import { selectShoppingCard } from "../../store/shoppingcard/selectors";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import CustomerDetails from "../../components/CustomerDetails";
 import CartItem from "../../components/CardItem";
 
 export default function Checkout() {
+  const dispatch = useDispatch();
+  const shoppingCard = useSelector(selectShoppingCard);
+
+  useEffect(() => {
+    dispatch(fetchShoppingCardById());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Checkout Page, awesome!</h1>
