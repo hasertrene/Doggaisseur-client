b7c815cf592c115b2e0cabee3f57c90a8de234bf
Tonic78 <guymertz@gmaiL.com>
2020-04-07 12:27:07 +0200

    Created basic component with hard coded data

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import CustomerDetails from "../../components/CustomerDetails";
+import CartItem from "../../components/CardItem";
 
 export default function Checkout() {
   return (
     <div>
       <h1>Checkout Page, awesome!</h1>
-      <Container>What's in my cart: item1, item2, item3</Container>
+      <Container>
+        What's in my cart:
+        <CartItem />
+      </Container>
       <Container>
         <CustomerDetails />
       </Container>
