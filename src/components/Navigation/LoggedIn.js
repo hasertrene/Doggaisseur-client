1f7c96fe208c20ec6ae737bb00f2a60a72385aec
Cherfez <chervasev.leeuwen@gmail.com>
2020-04-08 10:07:09 +0200

    checkout button in nav is working and removed warnings in nav

--- src/components/Navigation/LoggedIn.js
+++ src/components/Navigation/LoggedIn.js
@@ -5,15 +5,18 @@ import Button from "react-bootstrap/Button";
 import { selectUser } from "../../store/user/selectors";
 import Nav from "react-bootstrap/Nav";
 import { FiShoppingCart } from "react-icons/fi";
+import { useHistory } from "react-router-dom";
 
 export default function LoggedIn() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
+  const history = useHistory();
+
   return (
     <>
-      {/* Cant login yet, so see if it works later */}
       <div style={{ padding: ".5rem 1rem" }}>
-        <FiShoppingCart /> <span>1</span> <Button>Check Out</Button>
+        <FiShoppingCart /> <span>1</span>{" "}
+        <Button onClick={() => history.push("/checkout")}>Check Out</Button>
       </div>
       <Nav.Item style={{ padding: ".5rem 1rem" }}>Hi, {user.name}</Nav.Item>
       <Button onClick={() => dispatch(logOut())}>Logout</Button>
